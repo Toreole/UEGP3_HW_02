9e3eab5cedc54c62530dd584fe481583c1d2d64a
Toreole <toreole.official@gmail.com>
2020-05-29 18:30:00 +0200

    Rework FootstepAudioHandler to AnimationAudioHandler

---
+++ UEGP3Unity_HW2/Assets/Code/PlayerSystem/AnimationAudioHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UEGP3.Core;
+using UnityEngine;
+
+namespace UEGP3.PlayerSystem
+{
+	[RequireComponent(typeof(AudioSource))]
+	public class AnimationAudioHandler : MonoBehaviour
+	{
+		private AudioSource _audioSource;
+
+		private void Awake()
+		{
+			_audioSource = GetComponent<AudioSource>();
+		}
+
+        //animation event.
+        void PlaySound(ScriptableAudioEvent sound)
+        {
+            sound.Play(_audioSource);
+        }
+	}
+}
